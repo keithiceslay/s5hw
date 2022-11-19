b512b0f92c176de6c69f3853fd73980f359be3b9
Keith <4restrei@gmail.com>
2022-11-19 09:28:42 +0300

    sdfg

---
+++ pr2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива");
+int size = int.Parse(Console.ReadLine());
+int[] numbers = new int[size];
+FillArray(numbers);
+int sum = 0;
+
+for (int z = 0; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
+
+    Console.WriteLine($"Сумма элементов на нечётных позициях = {sum}");
+
+void FillArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
